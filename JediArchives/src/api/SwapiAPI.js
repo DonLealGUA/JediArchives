88f333f88c211e9782d207041b0a10c842b5a3ee
kristoffer leal <89843271+DonLealGUA@users.noreply.github.com>
2024-05-28 11:49:14 +0200

    Divided API call into 2 methods
    1 for fetching all characters
    2 for fetching specific characters

--- JediArchives/src/api/SwapiAPI.js
+++ JediArchives/src/api/SwapiAPI.js
@@ -1,4 +1,5 @@
 import { axios } from "@pipedream/platform"
+
 export default defineComponent({
   props: {
     swapi: {
@@ -6,9 +7,18 @@ export default defineComponent({
       app: "swapi",
     }
   },
-  async run({steps, $}) {
-    return await axios($, {
-      url: `https://swapi.dev/api/films/1/`,
-    })
+  methods: {
+    async getAllCharacters($) {
+      const response = await axios($, {
+        url: `https://swapi.dev/api/people/`,
+      });
+      return response.results;
+    },
+    async getCharacterById($, id) {
+      const response = await axios($, {
+        url: `https://swapi.dev/api/people/${id}/`,
+      });
+      return response;
+    },
   },
 })
