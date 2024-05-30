23558810f53a6dedf83d95bc82f93af0892aaafe
kristoffer leal <89843271+DonLealGUA@users.noreply.github.com>
2024-05-30 15:26:41 +0200

    added a loader animation

--- JediArchives/src/components/grid.jsx
+++ JediArchives/src/components/grid.jsx
@@ -6,6 +6,7 @@ import {
   Image,
   Stack,
   Heading,
+  Spinner 
 } from '@chakra-ui/react';
 import { swapiApi } from '../api/SwapiAPI';
 import CustomModal from '../components/modal';
@@ -13,10 +14,12 @@ import CustomModal from '../components/modal';
 function CustomGrid({ category }) {
   const [data, setData] = useState([]);
   const [selectedUrl, setSelectedUrl] = useState(null);
+  const [loading, setLoading] = useState(true); 
 
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setLoading(true); 
         let response;
         switch (category) {
           case 'characters':
@@ -40,6 +43,8 @@ function CustomGrid({ category }) {
         setData(response);
       } catch (error) {
         console.error('Error fetching data:', error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -51,28 +56,46 @@ function CustomGrid({ category }) {
   };
 
   return (
-    <div>
-      <Grid templateColumns="repeat(5, 1fr)" gap={6} padding={'80px'} paddingTop="auto">
-        {data.map((item, index) => (
-          <GridItem key={index} w="100%" h="300px">
-            <Box
-              bg={"blue.500"}
-              backgroundImage={process.env.PUBLIC_URL + '/assets/images/galaxia.png'}
-              borderRadius="lg"
-              p={4}
-              onClick={() => handleItemClick(item)}
-              style={{ cursor: 'pointer' }}
-            >
-              <Image src={process.env.PUBLIC_URL + `/assets/images/${category}/${item.name || item.title}.jpg`} borderRadius="lg" style={{ height: '230px', width: '300px' }} />
-              <Stack mt="6" spacing="3">
-                <Heading size="md" align="center" color={"white"}>
-                  {item.name || item.title}
-                </Heading>
-              </Stack>
-            </Box>
-          </GridItem>
-        ))}
-      </Grid>
+    <div style={{ position: 'relative', minHeight: '100vh' }}>
+      {loading && ( 
+        <div style={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)'
+        }}>
+          <Spinner
+            thickness='4px'
+            speed='0.65s'
+            emptyColor='gray.200'
+            color='blue.500'
+            size='xl'
+          />
+        </div>
+      )}
+      {!loading && (
+        <Grid templateColumns="repeat(5, 1fr)" gap={6} padding={'80px'} paddingTop="auto">
+          {data.map((item, index) => (
+            <GridItem key={index} w="100%" h="300px">
+              <Box
+                bg={"blue.500"}
+                backgroundImage={process.env.PUBLIC_URL + '/assets/images/galaxia.png'}
+                borderRadius="lg"
+                p={4}
+                onClick={() => handleItemClick(item)}
+                style={{ cursor: 'pointer' }}
+              >
+                <Image src={process.env.PUBLIC_URL + `/assets/images/${category}/${item.name || item.title}.jpg`} borderRadius="lg" style={{ height: '230px', width: '300px' }} />
+                <Stack mt="6" spacing="3">
+                  <Heading size="md" align="center" color={"white"}>
+                    {item.name || item.title}
+                  </Heading>
+                </Stack>
+              </Box>
+            </GridItem>
+          ))}
+        </Grid>
+      )}
       {selectedUrl && (
         <CustomModal url={selectedUrl} onClose={() => setSelectedUrl(null)} />
       )}
