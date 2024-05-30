02dc7a6c56743d0954a25c2d1424d48f78710b2e
kristoffer leal <89843271+DonLealGUA@users.noreply.github.com>
2024-05-30 15:16:34 +0200

    Expanded the Swapi API to include all pages and other categories

--- JediArchives/src/api/SwapiAPI.js
+++ JediArchives/src/api/SwapiAPI.js
@@ -6,10 +6,102 @@ const swapiApi = {
   */ 
   getAllCharacters: async () => {
     try {
-      const response = await axios.get(`https://swapi.dev/api/people/`);
-      return response.data.results;
+      let characters = [];
+      let url = `https://swapi.dev/api/people/`;
+      
+      while (url) {
+        const response = await axios.get(url);
+        characters = characters.concat(response.data.results);
+        url = response.data.next; 
+      }
+      
+      return characters;
     } catch (error) {
       console.error("Error fetching characters:", error);
+      return [];
+    }
+  },
+
+  /**
+  * API call to Swapi to get all Planets
+  */ 
+    getAllPlanets: async () => {
+      try {
+        let planets = [];
+        let url = `https://swapi.dev/api/planets/`;
+        
+        while (url) {
+          const response = await axios.get(url);
+          planets = planets.concat(response.data.results);
+          url = response.data.next;
+        }
+        
+        return planets;
+      } catch (error) {
+        console.error("Error fetching planets:", error);
+        return [];
+      }
+    },
+
+  /**
+  * API call to Swapi to get all StarShips
+  */ 
+  getAllStarShips: async () => {
+    try {
+      let starships = [];
+      let url = `https://swapi.dev/api/starships/`;
+      
+      while (url) {
+        const response = await axios.get(url);
+        starships = starships.concat(response.data.results);
+        url = response.data.next;
+      }
+      
+      return starships;
+    } catch (error) {
+      console.error("Error fetching starships:", error);
+      return [];
+    }
+  },
+
+  /**
+  * API call to Swapi to get all vehicles
+  */ 
+    getAllVehicles: async () => {
+      try {
+        let vehicles = [];
+        let url = `https://swapi.dev/api/vehicles/`;
+        
+        while (url) {
+          const response = await axios.get(url);
+          vehicles = vehicles.concat(response.data.results);
+          url = response.data.next;
+        }
+        
+        return vehicles;
+      } catch (error) {
+        console.error("Error fetching vehicles:", error);
+        return [];
+      }
+    },
+
+  /**
+  * API call to Swapi to get all movies
+  */ 
+  getAllMovies: async () => {
+    try {
+      let movies = [];
+      let url = `https://swapi.dev/api/films/`;
+      
+      while (url) {
+        const response = await axios.get(url);
+        movies = movies.concat(response.data.results);
+        url = response.data.next;
+      }
+      
+      return movies;
+    } catch (error) {
+      console.error("Error fetching movies:", error);
       return [];
     }
   },
